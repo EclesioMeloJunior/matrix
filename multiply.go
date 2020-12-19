9a4df9e6ac7a4a89f60156feaa06dc6439638d1e
eclesiomelojunior <eclesiomelo.1@gmail.com>
2020-12-19 12:50:34 -0300

    chore: Add multiply matrix

--- multiply.go
+++ multiply.go
@@ -1,5 +1,9 @@
 package matrix
 
+import (
+	"errors"
+)
+
 // MultplyByScalar will multply a matrix by a single value
 func (m *Matrix) MultplyByScalar(v int64) {
 	for i := 0; i < m.Rows(); i++ {
@@ -7,4 +11,28 @@ func (m *Matrix) MultplyByScalar(v int64) {
 			(*m)[i][j] = (*m)[i][j] * v
 		}
 	}
+}
+
+// Multiply will multiply 2 matrices and return a new matrix
+func (m *Matrix) Multiply(o *Matrix) (*Matrix, error) {
+	if m.Cols() != o.Rows() {
+		return nil, errors.New("The matrix has not the same rows and columns")
+	}
+
+	r := new(Matrix)
+	*r = make([][]int64, m.Rows())
+
+	for i := 0; i < m.Rows(); i++ {
+		(*r)[i] = make([]int64, o.Cols())
+
+		for j := 0; j < o.Cols(); j++ {
+
+			for k := 0; k < o.Rows(); k++ {
+				v := (*m)[i][k] * (*o)[k][j]
+				(*r)[i][j] = (*r)[i][j] + v
+			}
+		}
+	}
+
+	return r, nil
 }
--- multiply_test.go
+++ multiply_test.go
@@ -27,3 +27,53 @@ func TestMulltiplyByScalar(t *testing.T) {
 
 	assert.Equal(t, expected, a)
 }
+
+func TestMultiplyByMatrix(t *testing.T) {
+	a := matrix.Matrix(
+		[][]int64{
+			{2, 5, 9},
+			{3, 6, 8},
+		},
+	)
+
+	b := matrix.Matrix(
+		[][]int64{
+			{2, 7},
+			{4, 3},
+			{5, 2},
+		},
+	)
+
+	expected := matrix.Matrix(
+		[][]int64{
+			{69, 47},
+			{70, 55},
+		},
+	)
+
+	m, err := a.Multiply(&b)
+
+	assert.Nil(t, err)
+	assert.Equal(t, &expected, m)
+}
+
+func benchmarkMultiplyMatrix(ra, ca, rb, cb int, b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		a := matrix.RandInt(ra, ca)
+		b := matrix.RandInt(rb, cb)
+
+		a.Multiply(b)
+	}
+}
+
+func BenchmarkMultiply3by5Times4by3(b *testing.B) {
+	benchmarkMultiplyMatrix(3, 5, 4, 3, b)
+}
+
+func BenchmarkMultiply10by20Times20by60(b *testing.B) {
+	benchmarkMultiplyMatrix(10, 20, 20, 60, b)
+}
+
+func BenchmarkMultiply100by200Times200by100(b *testing.B) {
+	benchmarkMultiplyMatrix(100, 200, 200, 100, b)
+}
